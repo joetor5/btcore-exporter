0d2f8b7f29fcd99e5d25a1fb4dc9923798d2b8c9
Joel Torres <joetor5@icloud.com>
2024-08-13 07:53:34 -0500

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -20,9 +20,14 @@ echo -e "#!$SHELL\n" > $BOOT_SCRIPT_PATH
 echo ". $HOME/$SHRC" >> $BOOT_SCRIPT_PATH
 echo -e "$EXPORTER_PY $EXPORTER_PATH &\n" >> $BOOT_SCRIPT_PATH
 chmod +x $BOOT_SCRIPT_PATH
-echo "Boot script generated at $BOOT_SCRIPT_PATH"
+if [ -e $BOOT_SCRIPT_PATH ]; then
+    echo "Boot script generated at $BOOT_SCRIPT_PATH"
+else
+    echo "Error generating boot script at $BOOT_SCRIPT_PATH"
+    exit 1
+fi
 
-if [ $(crontab -l | grep "$BOOT_SCRIPT" > /dev/null; echo $?) != 0 ]; then
+if [ $(crontab -l | grep "$BOOT_SCRIPT" >/dev/null 2>&1; echo $?) != 0 ]; then
     echo "Updating crontab..."
     crontab -l > $CRON_TMP
     echo $CRON_JOB >> $CRON_TMP
